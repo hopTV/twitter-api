23d2f3b89aaec5ca843aaf58a1587cd26d3969cb
hopdev <hoptran.dev@gmail.com>
2023-09-05 17:52:16 +0700

    action upload

---
+++ server/src/utils/files.ts
@@ -0,0 +1,41 @@
+import { Request } from 'express'
+import { File } from 'formidable'
+import { UPLOAD_IMAGE_TEMP_DIR } from '~/constants/dir'
+
+export const handleUploadImage = async (req: Request) => {
+  const formidable = (await import('formidable')).default
+  const form = formidable({
+    uploadDir: UPLOAD_IMAGE_TEMP_DIR,
+    maxFiles: 4,
+    keepExtensions: true,
+    maxFileSize: 300 * 1024, // 300KB
+    maxTotalFileSize: 300 * 1024 * 4,
+    filter: function ({ name, originalFilename, mimetype }) {
+      const valid = name === 'image' && Boolean(mimetype?.includes('image/'))
+      if (!valid) {
+        form.emit('error' as any, new Error('File type is not valid') as any)
+      }
+      return valid
+    }
+  })
+  const result = new Promise<File[]>((resolve, reject) => {
+    form.parse(req, (err, fields, files) => {
+      if (err) {
+        return reject(err)
+      }
+      if (!Boolean(files.image)) {
+        return reject(new Error('File is empty'))
+      }
+      resolve(files.image as File[])
+      console.log(files.image)
+    })
+  })
+
+  return result
+}
+
+export const getNameFromFullname = (fullname: string) => {
+  const namearr = fullname.split('.')
+  namearr.pop()
+  return namearr.join('')
+}
